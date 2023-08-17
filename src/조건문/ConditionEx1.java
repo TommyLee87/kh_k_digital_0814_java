d29b5953788723edd519e9d3f683d7f1238a0a12
TommyLee <kokokorkr@naver.com>
2023-08-17 15:09:15 +0900

    조건문 반영

--- src/조건문/ConditionEx1.java
+++ src/조건문/ConditionEx1.java
@@ -1,11 +1,11 @@
 package 조건문;
+// 조건을 만족하는 경우와 만족하지 않는 경우로 나누어 수행하는 것
 
 import java.util.Scanner;
 
-// 조건문은 조건을 만족하는 경우와 만족하지 않는 경우로 나누어 수행하는 것
 public class ConditionEx1 {
     public static void main(String[] args) {
-        //if ~ else : 참인 경우와 거짓인 경우를 나누어서 수행
+        //if ~ else : 참인 경우와 거짓인 경우를 나누어 수행
 //        System.out.println("정수를 입력 하세요 : ");
 //        Scanner sc = new Scanner(System.in);
 //        int num = sc.nextInt();
@@ -27,11 +27,11 @@ public class ConditionEx1 {
         char ch = sc.next().charAt(0);
         if (ch >= 'a' && ch <= 'z') {
             System.out.println(ch + "는 소문자 입니다.");
-            } else if(ch >= 'A' && ch <= 'Z') {
+        } else if(ch >= 'A' && ch <= 'Z') {
             System.out.println(ch + "는 대문자 입니다.");
-            } else {
+        } else {
             System.out.println(ch + "는 알파벳이 아닙니다.");
-            }
+        }
 
     }
 }
