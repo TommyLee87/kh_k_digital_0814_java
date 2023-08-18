238602add1e94cfdb2b39406bd64ae5fd77ba618
TommyLee <kokokorkr@naver.com>
2023-08-18 11:25:28 +0900

    이중 for

--- src/중첩for문/DoubleForEx1.java
+++ src/중첩for문/DoubleForEx1.java
@@ -2,7 +2,7 @@ package 중첩for문;
 // 이중 반복문
 public class DoubleForEx1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {     //별찍기
             // System.out.printf("|i=%d|",i);
             for (int j = 0; j<10; j++) {
                 System.out.print("*");
