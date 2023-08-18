4d9c643748141e064358667ca760bae8e2e46e8b
TommyLee <kokokorkr@naver.com>
2023-08-18 13:27:18 +0900

    연습문제풀이

--- src/연습문제/SizeCompare.java
+++ src/연습문제/SizeCompare.java
@@ -2,7 +2,7 @@ package 연습문제;
 
 import java.util.Scanner;
 
-public class Size {
+public class SizeCompare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("세 자리의 정수를 입력하세요 : ");
