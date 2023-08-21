d7a6e3b637649c5f2bfd9a75806ac673dd42f529
TommyLee <kokokorkr@naver.com>
2023-08-21 16:23:25 +0900

    상근날드

--- src/알고리즘문제_초급/SGnald.java
+++ src/알고리즘문제_초급/SGnald.java
@@ -1,5 +1,5 @@
 package 알고리즘문제_초급;
-//상근날드
+// 상근날드
 // 문제 요약 : 햄버거3개와 음료2개를 연속으로 입력받아, 햄버거 중 가장 작은 값, 음료 2개 중 작은 값을 찾음
 // 찾은 값을 더하고 세트 메뉴 50원 할인 적용
 import java.util.Scanner;
@@ -8,7 +8,7 @@ public class SGnald {
     public static void main(String[] args) {
         // 5개의 값을 입력받기 위해 배열 생성
         int[] price = new int[5];
-        //키볻 입력을 받기 위해 스캐너 객체 생성
+        // 키보드 입력을 받기 위해 스캐너 객체 생성
         Scanner sc = new Scanner(System.in);
 
         System.out.print("가격 입력 : ");
