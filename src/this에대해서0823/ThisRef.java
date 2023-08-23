fc9d58786515282b163839a41e0cbdb54b730ddf
TommyLee <kokokorkr@naver.com>
2023-08-23 10:28:25 +0900

    this

---
+++ src/this에대해서0823/ThisRef.java
@@ -0,0 +1,11 @@
+package this에대해서0823;
+// this : 자기 자신의 객체를 참조하는데 사용하는 참조변수
+// this() : 생성자 내에서 다른 생성자를 호출하는데 사용
+
+public class ThisRef {
+    public static void main(String[] args) {
+        Car car = new Car("Santafe");
+
+
+    }
+}
