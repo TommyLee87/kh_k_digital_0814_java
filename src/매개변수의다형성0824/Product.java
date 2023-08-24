0cf6f2dbc7e804274e0918a07a59d4d2559a3d0d
TommyLee <kokokorkr@naver.com>
2023-08-24 11:41:58 +0900

    매개변수의 다형성 money/point

---
+++ src/매개변수의다형성0824/Product.java
@@ -0,0 +1,26 @@
+package 매개변수의다형성0824;
+
+public class Product {
+    int price;
+    int bonusPoint;
+}
+
+class TV extends Product {
+    TV() {
+        this.price = 100;
+        this.bonusPoint = 10;
+
+    }
+}
+class Computer extends Product {
+    Computer() {
+        this.price = 200;
+        this.bonusPoint = 20;
+    }
+}
+class Audio extends Product {
+    Audio() {
+        this.price = 120;
+        this.bonusPoint = 12;
+    }
+}
