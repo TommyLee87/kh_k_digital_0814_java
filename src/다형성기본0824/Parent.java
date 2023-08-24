fadbc02929b32711e03422bc36bcfa2201520b73
TommyLee <kokokorkr@naver.com>
2023-08-24 11:42:25 +0900

    다형성 기본

--- src/다형성기본0824/Parent.java
+++ src/다형성기본0824/Parent.java
@@ -1,7 +1,7 @@
 package 다형성기본0824;
 
 public class Parent {
-    int num = 100000; // 상속관계와 같은 패키지내에서 접근 가능
+    protected int num; // 상속관계와 같은 패키지내에서 접근 가능
     public void display() {
         System.out.println("부모 클래스 메소드");
 
