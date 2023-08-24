99a2b1ba64d05205ae50432dd368b3e7e8b07dbf
TommyLee <kokokorkr@naver.com>
2023-08-24 17:02:15 +0900

    정적 메소드

--- src/정적멤버와메소드0824/Bank.java
+++ src/정적멤버와메소드0824/Bank.java
@@ -8,7 +8,7 @@ public class Bank {
         count++;
         this.bank = name;
         this.account = account;
-        System.out.println(bank + "은행에 계좌를 개설합니다. 잔액은" + this.account + "입니다.");
+        System.out.println(bank + "은행에 계좌를 개설합니다. 잔액은 " + this.account + "입니다.");
     }
     public static int getCount() { // 정적 메소드, 객체로 만들어지지 않음
         return count;
@@ -22,10 +22,11 @@ public class Bank {
             System.out.println("잔액이 부족합니다.");
         } else {
             account -= with;
+            System.out.println(with + "을 출금 했습니다.");
         }
     }
     public void viewAccount() {
-        System.out.println("현재 잔액은 + " + account + "입니다.");
+        System.out.println("현재 잔액은 " + account + "입니다.");
     }
 }
 
