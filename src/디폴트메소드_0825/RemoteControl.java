ace52babe6a4f76ddde16d82070d642acf130fad
TommyLee <kokokorkr@naver.com>
2023-08-25 16:41:03 +0900

    디폴트 메소드 / Audio,TV

---
+++ src/디폴트메소드_0825/RemoteControl.java
@@ -0,0 +1,18 @@
+package 디폴트메소드_0825;
+
+public interface RemoteControl {
+    int MAX_VOLUME = 100; // final static 추가 됨(대문자)
+    int MIN_VOLUME = 0;
+    void turnON(); // 자동으로 public abstract 추가 됨
+    void turnOFF();
+    void setVolume(int vol);
+// 디폴트 메소드는 JDK 8 이후에 추가 됨
+    default void setMute(boolean mute) {
+        if(mute) System.out.println("음소거 처리 합니다.");
+        else System.out.println("음소거 상태를 해제합니다.");
+
+    }
+    static void changeBattery() {
+        System.out.println("건전지를 교환 합니다.");
+    }
+}
---
+++ src/디폴트메소드_0825/Television.java
@@ -0,0 +1,37 @@
+package 디폴트메소드_0825;
+
+public class Television implements RemoteControl {
+    private int volume;
+
+    @Override
+    public void turnON() {
+        System.out.println("TV를 켭니다.");
+    }
+
+    @Override
+    public void turnOFF() {
+        System.out.println("TV를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int vol) {
+        if (vol > RemoteControl.MAX_VOLUME) {
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if (vol < RemoteControl.MIN_VOLUME) {
+            this.volume = RemoteControl.MIN_VOLUME;
+        } else {
+            this.volume = vol;
+        }
+        System.out.println("현재 TV 볼륨 : " + this.volume);
+    }
+    void getInfo() {
+        System.out.println("TV 입니다.");
+        System.out.println("현재 TV 볼륨은 " + volume + "입니다.");
+    }
+
+    @Override
+    public void setMute(boolean mute) {
+        if(mute) System.out.println("TV를 음소거 처리합니다.");
+        else System.out.println("TV 음소거를 해제합니다.");
+    }
+}
