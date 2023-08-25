3d1a9d9ad66821133ecc2b5ff4a8e81383b22787
TommyLee <kokokorkr@naver.com>
2023-08-25 17:14:54 +0900

    디폴트 메소드 / Audio,TV

--- src/디폴트메소드_0825/RemoteControl.java
+++ src/디폴트메소드_0825/RemoteControl.java
@@ -8,11 +8,11 @@ public interface RemoteControl {
     void setVolume(int vol);
 // 디폴트 메소드는 JDK 8 이후에 추가 됨
     default void setMute(boolean mute) {
-        if(mute) System.out.println("음소거 처리 합니다.");
+        if(mute) System.out.println("음소거 처리합니다.");
         else System.out.println("음소거 상태를 해제합니다.");
 
     }
     static void changeBattery() {
-        System.out.println("건전지를 교환 합니다.");
+        System.out.println("건전지를 교환합니다.");
     }
 }
