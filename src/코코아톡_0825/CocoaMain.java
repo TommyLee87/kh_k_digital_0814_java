5feec9eac8fb35c552af27050661b2413d81f181
TommyLee <kokokorkr@naver.com>
2023-08-25 15:39:20 +0900

    코코아톡

---
+++ src/코코아톡_0825/CocoaMain.java
@@ -0,0 +1,18 @@
+package 코코아톡_0825;
+
+import java.util.Scanner;
+
+public class CocoaMain {
+    public static void main(String[] args) {
+        CocoaTalk cocoaTalk = new CocoaTalk("도리도리");
+        cocoaTalk.writeMsg("오늘 아침에 비가 많이 왔어요^^");
+        NetAdapter adapter;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("네트워크 [1]WiFi [2]5G");
+        int sel = sc.nextInt();
+        if(sel == 1) adapter = new WiFi();
+        else adapter = new FiveG();
+        cocoaTalk.send(adapter);
+
+    }
+}
---
+++ src/코코아톡_0825/NetAdapter.java
@@ -0,0 +1,30 @@
+package 코코아톡_0825;
+
+public interface NetAdapter {
+    void connect();
+    void send(String msg);
+
+}
+class WiFi implements NetAdapter {
+
+    @Override
+    public void connect() {
+        System.out.println("WiFi에 연결 되었습니다.");
+    }
+
+    @Override
+    public void send(String msg) {
+        System.out.println("WiFi >> " + msg);
+    }
+}
+class FiveG implements NetAdapter {
+
+    @Override
+    public void connect() {
+        System.out.println("FiveG에 연결 되었습니다.");
+    }
+    @Override
+    public void send(String msg) {
+        System.out.println("5G >> " + msg);
+    }
+}
