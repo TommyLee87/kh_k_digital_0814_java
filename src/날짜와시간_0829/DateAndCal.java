5d52a8b70ce58b78d1a8a91d053619cf9694d89a
TommyLee <kokokorkr@naver.com>
2023-08-29 16:40:58 +0900

    Date format

--- src/날짜와시간_0829/DateAndCal.java
+++ src/날짜와시간_0829/DateAndCal.java
@@ -1,6 +1,7 @@
 package 날짜와시간_0829;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 // Date() : 운영체제로부터 날짜와 시간 정보를 가져와 원하는 포맷으로 출력, 객체를 생성해야 함.
@@ -15,7 +16,7 @@ public class DateAndCal {
         // hh : 12시간제
         // mm : 분
         // ss : 초
-        SimpleDateFormat f1,f2,f3,f4,f5,f6,f7;
+        SimpleDateFormat f1, f2, f3, f4, f5, f6, f7, f8;
         f1 = new SimpleDateFormat("yyyy-MM-dd");
         f2 = new SimpleDateFormat("yy/MM/dd");
         f3 = new SimpleDateFormat("yyyy년 MM월 dd일");
@@ -23,14 +24,40 @@ public class DateAndCal {
         f5 = new SimpleDateFormat("오늘은 M월의 W번째 주, d번째 날입니다");
         f6 = new SimpleDateFormat("오늘은 yyyy년의 w주차 입니다.");
         f7 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        f8 = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분 ss초");
 
-        System.out.println(f1.format(now));
-        System.out.println(f2.format(now));
-        System.out.println(f3.format(now));
-        System.out.println(f4.format(now));
-        System.out.println(f5.format(now));
-        System.out.println(f6.format(now));
-        System.out.println(f7.format(now));
+//        System.out.println(f1.format(now));
+//        System.out.println(f2.format(now));
+//        System.out.println(f3.format(now));
+//        System.out.println(f4.format(now));
+//        System.out.println(f5.format(now));
+//        System.out.println(f6.format(now));
+//        System.out.println(f7.format(now));
+        System.out.println(f8.format(now));
+
+//        Calendar cal = Calendar.getInstance(); // 운영 체제로부터 시간을 가지고 옴
+//        System.out.println(cal.get(Calendar.YEAR));
+//        System.out.println(cal.get(Calendar.MONTH) + 1);  // 인덱스 배열임을 유의.
+//        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+//        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
+//        System.out.println(cal.get(Calendar.AM_PM));
+//        System.out.println(cal.get(Calendar.HOUR));
+//        System.out.println(cal.get(Calendar.MINUTE));
+//        System.out.println(cal.get(Calendar.SECOND));
+
+        //문제 : Calendar 클래스로 yyyy년 MM월 dd일 HH시 mm분 ss초 포맷과 동일하게 출력 하기
+
+        Calendar date = Calendar.getInstance();
+        int year = date.get(Calendar.YEAR);
+        int mon = date.get(Calendar.MONTH)+1;
+        int day = date.get(Calendar.DAY_OF_MONTH);
+        int hour = date.get(Calendar.HOUR);
+        int isPm = date.get(Calendar.AM_PM);
+        int min = date.get(Calendar.MINUTE);
+        int sec = date.get(Calendar.SECOND);
+        if (isPm == 1) hour += 12;
+        System.out.printf("%d년 %02d월 %02d일 %02d시 %02d분 %02d초\n", year, mon, day, hour, min, sec);
+
 
     }
 }
