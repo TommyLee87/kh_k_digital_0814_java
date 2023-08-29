1eccc67fa73d0c3994387bab5036f59649a9dc4a
TommyLee <kokokorkr@naver.com>
2023-08-29 14:09:51 +0900

    클론 오브젝트

---
+++ src/클론오브젝트/CloneObject.java
@@ -0,0 +1,13 @@
+package 클론오브젝트;
+
+public class CloneObject {
+    public static void main(String[] args) {
+        Member mem1 = new Member("Bear", "곰돌이사육사", "123456", 20, true);
+        Member mem2 = mem1.getMember(); // 얕은 복사가 일어나는 조건
+        mem1.viewInfo();
+        mem2.name = "KH정보교육원";
+        mem2.password = "KKKKKK";
+        mem2.viewInfo();
+        mem1.viewInfo();
+    }
+}
