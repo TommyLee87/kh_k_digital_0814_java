f23ddb854de3d59a3303ac2b2b6ca67b59ea894b
TommyLee <kokokorkr@naver.com>
2023-08-30 17:28:34 +0900

    Vector

--- src/Vector클래스_0830/VectorClass.java
+++ src/Vector클래스_0830/VectorClass.java
@@ -4,16 +4,26 @@ package Vector클래스_0830;
 // 하지만 ArrayList 대비 성능이 떨어짐
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
 public class VectorClass {
     public static void main(String[] args) {
-        List<NameCard> list = new Vector<>();
-        list.add(new NameCard("우영우", "WYW@gmail.com","010-1234-5678","변호사"));
+        List<NameCard> list = new Vector<>();  // Vextor 대신 ArrayList, LinkedList를 써도 무방
+        list.add(new NameCard("우영우", "WYW@gmail.com", "010-1234-5678", "변호사"));
+        list.add(new NameCard("동그라미", "ROUND@gmail.com", "010-4567-9018", "무직"));
+        list.add(new NameCard("이준호", "JHL@gmail.com", "010-1346-1792", "회사원"));
 
+        for (NameCard e : list) {
+            System.out.println("이름 : " + e.name);
+            System.out.println("메일 : " + e.mail);
+            System.out.println("연락처 : " + e.phone);
+            System.out.println("직업 : " + e.jobs);
+        }
     }
 }
+
 class NameCard {
     String name;
     String mail;
