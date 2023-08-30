083c04ea71a559ff6112d0bdd1202995cd7ebe4d
TommyLee <kokokorkr@naver.com>
2023-08-30 14:09:34 +0900

    List 예제1 / ArrayList

--- src/List예제1_0830/ArrayListEx1.java
+++ src/List예제1_0830/ArrayListEx1.java
@@ -5,11 +5,14 @@ import java.util.List;
 
 public class ArrayListEx1 {
     public static void main(String[] args) {
-        List<MenuInfo> menuInfo = new ArrayList<>();
-        menuInfo.add(new MenuInfo("Americano", 2500, "coffee", true));
-        menuInfo.add(new MenuInfo("Latte", 4000, "coffee", true));
-        menuInfo.add(new MenuInfo("Espresso", 2500, "coffee", true));
-        for(MenuInfo menu : menuInfo) {
+        // List 인터페이스의 참조 변수로 ArrayList 객체를 참조.
+        List<MenuInfo> menuInfoList = new ArrayList<>();
+        // add는 객체를 추가, 추가 할 때마다 크기가 변함.
+        MenuInfo menuInfo1 = new MenuInfo("Americano", 2500, "coffee", true);
+        menuInfoList.add(menuInfo1);
+        menuInfoList.add(new MenuInfo("Latte", 4000, "coffee", true));
+        menuInfoList.add(new MenuInfo("Espresso", 2500, "coffee", true));
+        for(MenuInfo menu : menuInfoList) {
             System.out.println(menu.getMenuInfo());
         }
     }
