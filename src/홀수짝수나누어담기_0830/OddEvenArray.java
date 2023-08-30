129f212e7cff35c574732fe3d8a0041743ac5268
TommyLee <kokokorkr@naver.com>
2023-08-30 15:36:35 +0900

    홀짝나누기 / 배열로 풀기

---
+++ src/홀수짝수나누어담기_0830/OddEvenArray.java
@@ -0,0 +1,31 @@
+package 홀수짝수나누어담기_0830;
+
+import java.util.Scanner;
+// 7개의 정수를 입력 받는 메소드
+// 홀수, 짝수 배열로 나누어 담는 메소드
+// 나뉸 홀수, 짝수를 출력하는 메소드
+// 입력 : 1 2 3 4 5 6 7
+// 홀수 : 1 3 5 7
+// 짝수 : 2 4 6
+
+public class OddEvenArray {
+    public static void main(String[] args) {
+        OddEvenFunc oddEvenFunc = new OddEvenFunc();
+        oddEvenFunc.inputFunc();
+        oddEvenFunc.oddEvenCalc();
+        oddEvenFunc.oddEvenOutput();
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
