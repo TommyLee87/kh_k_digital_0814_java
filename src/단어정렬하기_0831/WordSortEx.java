f95d6eeec802df43f9b0cb54f2d8ba014d969a20
TommyLee <kokokorkr@naver.com>
2023-09-01 09:21:00 +0900

    단어 정렬하기

--- src/단어정렬하기_0831/WordSortEx.java
+++ src/단어정렬하기_0831/WordSortEx.java
@@ -17,11 +17,12 @@ public class WordSortEx {
         int n = sc.nextInt(); // n의 개수 입력 받음.
 
         String[] word = new String[n];
-        sc.nextLine();
+        sc.nextLine(); // 버퍼 비우기
         for (int i = 0; i < word.length; i++) {
             word[i] = sc.nextLine();
         }
         // 입력 받은 문자열 배열의 중복 제거를 위해 set 컬렉션에 넣음
+        // asList() : 배열을 컬렉션에 넣을 때 사용하는 메소드
         HashSet<String> set = new HashSet<>(Arrays.asList(word));
         // 다시 배열로 되돌림, 원래 생성된 크기로 되돌림
         word = set.toArray(new String[0]);
@@ -30,9 +31,9 @@ public class WordSortEx {
             @Override
             public int compare(String o1, String o2) {
                 if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2); // 사전순 정렬
+                    return o1.compareTo(o2); // 길이가 같으면 사전순 정렬
                 } else {
-                    return o1.length() - o2.length(); // 만약 앞이 길면 양수
+                    return o1.length() - o2.length(); // 만약 앞이 길면 양수(양수가 반환되면 정렬)
                 }
             }
         });
