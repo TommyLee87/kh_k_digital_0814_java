9c0d24818e493ff1ef760cca7fafaa195c8d75b3
TommyLee <kokokorkr@naver.com>
2023-09-01 15:06:12 +0900

    쓰레드 만들기 예제1

--- src/쓰레드예제1_0901/ThMainEx1.java
+++ src/쓰레드예제1_0901/ThMainEx1.java
@@ -3,16 +3,46 @@ package 쓰레드예제1_0901;
 
 public class ThMainEx1 {
     public static void main(String[] args) {
+        // Thread 클래스를 상속 받아서 Thread를 생성
         Thread testTh = new CustomThread();
         testTh.start();
         System.out.println("Main에서 찍어봅니다.");
 
+        // Runnable 인터페이스를 상속 받아서 쓰레드를 만드는 방법
         Runnable runTask = new InterThread(); // 작업 내용을 가지고 있지만 실제 쓰레드는 아님
         Thread runTh1 = new Thread(runTask);
+        Thread runTh2 = new Thread(runTask);
         runTh1.start();
+        runTh2.start();
 
+        // 익명의 객체로 쓰레드를 만드는 방법
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                int sum = 0;
+                for (int i = 0; i <= 10; i++) {
+                    sum += i;
+                    System.out.println("상속받은 쓰레드 : " + sum);
+                }
+                System.out.println(Thread.currentThread() + "합 : " + sum);
+            }
+        };
+        Thread anonyTh1 = new Thread(task);
+        anonyTh1.start();
+
+        // 람다식으로 구현하기
+        Runnable task2 = () -> {
+            int sum = 0;
+            for (int i = 0; i <= 10; i++) {
+                sum += i;
+                System.out.println("상속받은 쓰레드 : " + sum);
+            }
+            System.out.println(Thread.currentThread() + "합 : " + sum);
+        };
     }
+
 }
+
 
 // 상속 받아서 쓰레드를 만드는 방법
 class CustomThread extends Thread {
@@ -21,12 +51,13 @@ class CustomThread extends Thread {
         int sum = 0;
         for (int i = 0; i <= 10; i++) {
             sum += i;
-            System.out.println("상속받은 쓰레드 : "+ sum);
+            System.out.println("상속받은 쓰레드 : " + sum);
         }
         System.out.println(Thread.currentThread() + "합 : " + sum);
 
     }
 }
+
 class InterThread implements Runnable {
 
     @Override
