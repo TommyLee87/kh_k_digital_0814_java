e43a77b706d1a23ba4c73426b72e4d650173d383
TommyLee <kokokorkr@naver.com>
2023-09-05 10:46:25 +0900

    ByteStream

---
+++ src/문자스트림_0905/CharStreamEx.java
@@ -0,0 +1,25 @@
+package 문자스트림_0905;
+// Writer : 문자열 기반 출력 스트림의 최상위 클래스, 추상클 래스
+
+import java.io.*;
+
+public class CharStreamEx {
+    public static void main(String[] args) throws IOException {
+//        Writer writer = new FileWriter("test.txt");
+//        char[] data = "곰도리464".toCharArray();
+//        for (int i = 0; i < data.length; i++) {
+//            writer.write(data[i]);
+//        }
+//        writer.write(data);
+//        writer.flush(); // 버퍼 비우기
+//        writer.close();
+
+        Reader reader = new FileReader("test.txt");
+        while (true) {
+            int readData = reader.read();
+            if (readData == -1) break;
+            System.out.print((char)readData);
+        }
+        reader.close();
+    }
+}
