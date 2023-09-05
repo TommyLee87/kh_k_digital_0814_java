5e4842d9b2aa42f432d978eaaf25c9eac49f8826
TommyLee <kokokorkr@naver.com>
2023-09-05 14:14:44 +0900

    직렬화연습

---
+++ src/직렬화연습_0905/SerializeEx.java
@@ -0,0 +1,52 @@
+package 직렬화연습_0905;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+// 직렬화 : 객체를 출력하기 위해서는 일렬의 연속적인 바이트로 변경해야 함
+public class SerializeEx {
+    public static void main(String[] args) {
+        writeList();
+        List<Board> list = readList();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for (Board b : list) {
+            System.out.printf("%d %s %s %s %s\n",
+                    b.getBno() + b.getTitle() + b.getContent() + b.getWriter() + sdf.format(b.getDate()));
+        }
+    }
+
+    public static void writeList() {
+        List<Board> list = new ArrayList<>();
+        list.add(new Board(1, "C++", "고성능언어", "우영우", new Date()));
+        list.add(new Board(2, "Java", "객체지향언어", "동그라미우", new Date()));
+        list.add(new Board(3, "Python", "간결한언어", "이준호", new Date()));
+
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+        try {
+            fos = new FileOutputStream("board.db");
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(list);
+            oos.flush();
+            oos.close();
+        } catch (IOException e) {}
+    }
+
+    public static List<Board> readList() {
+        try {
+            FileInputStream fis = new FileInputStream("board.db");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<Board> list = (List<Board>) ois.readObject();
+            return list;
+
+        } catch (IOException e) {
+        } catch (ClassNotFoundException e) {}
+        return null;
+
+    }
+}
