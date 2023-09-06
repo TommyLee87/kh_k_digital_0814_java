b9d1568ea2ed2727497a687c2c3df027bd1dc7cc
TommyLee <kokokorkr@naver.com>
2023-09-06 13:46:42 +0900

    자바 파이널 - 커피 메뉴 읽기 / 직렬화와 역직렬화

--- src/커피메뉴읽기_0905/CoffeeMenuRead.java
+++ src/커피메뉴읽기_0905/CoffeeMenuRead.java
@@ -2,7 +2,6 @@ package 커피메뉴읽기_0905;
 
 import 커피메뉴만들기_0831.MenuInfo;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
@@ -13,7 +12,7 @@ public class CoffeeMenuRead {
         FileInputStream fis = new FileInputStream("src/커피메뉴만들기_0831/coffee.bin");
         ObjectInputStream ois = new ObjectInputStream(fis);
         Map<String, MenuInfo> map = (Map<String, MenuInfo>) ois.readObject();
-        for(String e : map.keySet()) {
+        for (String key : map.keySet()) {
             System.out.println("메뉴 : " + map.get(key).getName());
             System.out.println("가격 : " + map.get(key).getPrice());
             System.out.println("분류 : " + map.get(key).getCategory());
@@ -21,4 +20,5 @@ public class CoffeeMenuRead {
             System.out.println("======================================");
         }
     }
+
 }
